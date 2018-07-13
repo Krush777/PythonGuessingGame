6fb1155085d55ac7f7bd1361078d01c3ed413a0a
cneymoss <cneymoss@cmlibrary.org>
2018-07-13 16:36:08 -0400

    Update guessinggame.py

--- guessinggame.py
+++ guessinggame.py
@@ -4,13 +4,13 @@ guess = int(raw_input("Guess a number between 1 and 99:"))
 while actual != "guess":
     print
     if guess < actual:
-        print "Your guess is too low!"
+        print ("Your guess is too low!")
         guess = int(raw_input("Enter an integer from 1 to 99:"))
     elif guess > actual:
-        print "Your guess is too high!"
+        print ("Your guess is too high!")
         guess = int(raw_input("Enter an integer from 1 to 99:"))
     else:
-        print "You guessed it!"
+        print ("You guessed it!")
         break
     print
 
